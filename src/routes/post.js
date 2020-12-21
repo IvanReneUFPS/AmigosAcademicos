155634b1842536be29beeebf0093e47c9cdda622
CristianCamargo04 <cristianfernandocc@ufps.edu.co>
2020-12-21 15:58:18 -0500

    se filtra por materia

--- src/routes/post.js
+++ src/routes/post.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const router = Router();
 const path = require("path");
 const Post = require("../models/Post");
+const Materia = require("../models/Materia");
 
 router.post("/nueva", async (req, res) => {
     const { titulo, materia, contenido } = req.body;
@@ -15,6 +16,24 @@ router.post("/nueva", async (req, res) => {
         return res.redirect('/');
     }
     return res.redirect('/');
+});
+
+router.get("/materia/:_id", async (req, res)=>{
+    if (req.user) {
+        const _id = req.params._id;
+        const materias = await Materia.find().lean();
+        const preguntas = await Post.find({materia:_id}).lean();
+        return res.render("index", {
+            title: "Inicio",
+            user: req.user,
+            esAdmin: () => req.user.rol === "Administrador",
+            materias,
+            preguntas,
+        });
+    }
+    return res.render("login", {
+        title: "Iniciar sesión",
+    });
 });
 
 module.exports = router;
